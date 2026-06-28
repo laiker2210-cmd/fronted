ec7bce9903a7c9d393cdae8418e2bad4c8bd22ea
laiker <mr.dj-alex2010@yandex.ru>
2026-06-28 23:39:03 +0900

    dz36

--- DZ/dz36/dz.js
+++ DZ/dz36/dz.js
@@ -5,16 +5,17 @@
 // классы
 // задание - наследование 
 
+//родительский
 class Worker {
     constructor(name, surname, rate, days) {
-        this._name = name;
-        this._surname = surname;
-        this._rate = rate;
-        this._days = days;
+        this._name = name
+        this._surname = surname
+        this._rate = rate
+        this._days = days
     }
 
     get getFullName() {
-        return this._surname + " " + this._name;
+        return this._surname + " " + this._name
     }
 
     get name() {
@@ -38,10 +39,39 @@ class Worker {
     }
 }
 
-const worker = new Worker("Иван", "Иванов", 10, 31)
+const worker = new Worker("Василий", "Степанов", 10, 31)
 console.log(worker.name);
 console.log(worker.surname);
 console.log(worker.getFullName);
 console.log(worker.rate);
 console.log(worker.days);
 console.log(worker.getSalary());
+
+//дочерний
+
+class Boss extends Worker {
+    constructor(name, surname, rate, days, workers) {
+        super(name, surname, rate, days)
+        this.workers = workers
+    }
+
+    getSalary() {//переназначим
+        return (this.rate * this.days) * this.workers
+    }
+}
+
+const boss = new Boss("Иван", "Иванов", 10, 31, 10);
+console.log(boss.name);
+console.log(boss.surname);
+console.log(boss.getFullName);
+console.log(boss.rate);
+console.log(boss.days);
+console.log(boss.workers);
+console.log(boss.getSalary());
+
+
+
+
+
+
+
