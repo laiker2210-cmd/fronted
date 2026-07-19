0488477236b86878c4a5f3d91671c0778b06279e
laiker <mr.dj-alex2010@yandex.ru>
2026-07-19 23:02:57 +0900

    NewTetris

--- Lesson/lesson_37_38/tetris/script.js
+++ Lesson/lesson_37_38/tetris/script.js
@@ -6,8 +6,8 @@
 
 class Game {
     //игровое поле - высота и ширина
-    // playfield = [
-    //     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    // playfield = 
+    //     [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     //     [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,8 +26,18 @@ class Game {
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    // ]
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
+
+
+    static points = {
+        "1": 40,
+        "2": 100,
+        "3": 300,
+        "4": 1200
+    }
+
+    score = 0;
+    lines = 0;
 
     playfield = this.creatPlayfield() //игровое поле - высота и ширина
 
@@ -43,6 +53,11 @@ class Game {
     //}
 
     nextPiece = this.createPiece()
+
+    getLevel() {
+        //return Math.floor(this.lines * 0.1)
+        return Math.floor(this.lines / 10) + 1
+    }
 
     getState() {//копия игрового поля
         const playfield = this.creatPlayfield()
@@ -65,6 +80,10 @@ class Game {
 
         }
         return {
+            score: this.score,
+            level: this.getLevel(),
+            lines: this.lines,
+            nextPiece: this.nextPiece,
             playfield
         }
 
@@ -84,14 +103,57 @@ class Game {
         }
     }
 
-    moveIsDawn() {//перемщение фигуры в низ
+    moveIsDown() {//перемщение фигуры в низ
         this.activePiece.y++
 
         if (this.hasCollision()) {
             this.activePiece.y--
-            this.lockPiace()
+            this.lockPiece()
+            const clearLines = this.clearLines()
+            this.updateScore(clearLines)
             this.updatePiece()
         }
+    }
+
+    updateScore(clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines] //* (this.getLevel()) // + 1
+            this.lines += clearedLines
+            console.log(this.score, this.lines, this.getLevel());
+
+        }
+    }
+
+    clearLines() {
+        const rows = 20
+        const colums = 10
+        let lines = []
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < colums; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfBlocks++
+                }
+            }
+
+            if (numberOfBlocks === 0) {
+                break
+            } else if (numberOfBlocks < colums) {
+                continue
+            } else { // numberOfBlocks === colums
+                lines.unshift(y)
+            }
+
+        }
+
+        for (let index of lines) {
+            this.playfield.splice(index, 1)
+            this.playfield.unshift(new Array(colums).fill(0))
+        }
+
+        return lines.length
     }
 
     hasCollision() {
@@ -107,7 +169,7 @@ class Game {
         return false
     }
 
-    lockPiace() {
+    lockPiece() {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece
 
         for (let y = 0; y < blocks.length; y++) {
@@ -164,7 +226,7 @@ class Game {
 
         const index = Math.floor(Math.random() * 7)
         const type = "IJLOSTZ"[index]
-        const piece = { x: 0, y: 0 }
+        const piece = {}
 
         switch (type) {
             case "I":
@@ -178,61 +240,71 @@ class Game {
             case "J":
                 piece.blocks = [
                     [0, 0, 0],
-                    [1, 1, 1],
-                    [0, 0, 1]
-
+                    [2, 2, 2],
+                    [2, 0, 0]
                 ]
                 break
             case "L":
                 piece.blocks = [
-                    [0, 0, 1],
-                    [1, 1, 1],
-                    [0, 0, 0]
-
+                    [0, 0, 0],
+                    [3, 3, 3],
+                    [0, 0, 3]
                 ]
                 break
             case "O":
                 piece.blocks = [
                     [0, 0, 0, 0],
-                    [0, 1, 1, 0],
-                    [0, 1, 1, 0],
+                    [0, 4, 4, 0],
+                    [0, 4, 4, 0],
                     [0, 0, 0, 0]
-
                 ]
                 break
             case "S":
                 piece.blocks = [
                     [0, 0, 0],
-                    [0, 1, 1],
-                    [1, 1, 0]
-
+                    [0, 5, 5],
+                    [5, 5, 0]
                 ]
                 break
             case "T":
                 piece.blocks = [
                     [0, 0, 0],
-                    [1, 1, 1],
-                    [0, 1, 0]
-
+                    [6, 6, 6],
+                    [0, 6, 0]
                 ]
                 break
             case "Z":
                 piece.blocks = [
                     [0, 0, 0],
-                    [1, 1, 0],
-                    [0, 1, 1]
-
+                    [7, 7, 0],
+                    [0, 7, 7]
                 ]
                 break
             default:
                 throw new Error("Неизыестный тип фигуры")
         }
 
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
+
+        piece.y = -1
+
         return piece
     }
 }
 
 class View {
+
+    static colors = {
+
+        "1": "cyan",
+        "2": "blue",
+        "3": "orange",
+        "4": "yellow",
+        "5": "green",
+        "6": "purple",
+        "7": "red"
+    }
+
     constructor(element, width, height, rows, colums) {
         this.element = element
         this.width = width
@@ -243,8 +315,21 @@ class View {
         this.canvas.height = this.height
         this.context = this.canvas.getContext("2d")
 
-        this.blockWidth = this.width / colums
-        this.blockHeight = this.height / rows
+        this.playfieldBorderWidth = 4
+        this.playfieldX = this.playfieldBorderWidth
+        this.playfieldY = this.playfieldBorderWidth
+        this.playfieldWidth = this.width * 2 / 3
+        this.playfieldHeight = this.height
+        this.playfieldInnerWidth = this.playfieldWidth - this.playfieldBorderWidth * 2
+        this.playfieldInnerHeight = this.playfieldHeight - this.playfieldBorderWidth * 2
+
+        this.blockWidth = this.playfieldInnerWidth / colums
+        this.blockHeight = this.playfieldInnerHeight / rows
+
+        this.panelX = this.playfieldWidth + 10
+        this.panelY = 0
+        this.panelWidth = this.width / 3
+        this.panelHeigth = this.height
 
         this.element.append(this.canvas)
     }
@@ -253,12 +338,14 @@ class View {
         this.context.clearRect(0, 0, this.width, this.height)
     }
 
-    render({ playfield }) {
+    render(state) { // было{ playfield }
         this.cleareScrean()
-        this.renderPlayfield(playfield)
+        this.renderPlayfield(state)
+        this.renderPanel(state)
+
     }
 
-    renderPlayfield(playfield) {
+    renderPlayfield({ playfield }) {
         // Проходим по каждой строке (y)
         for (let y = 0; y < playfield.length; y++) {
             const line = playfield[y]// текущая строка
@@ -268,33 +355,122 @@ class View {
                 const block = line[x]
                 // Если блок ≠ 0 (заполнен)
                 if (block) {
-                    this.context.fillStyle = "red"
-                    this.context.strokeStyle = "black"
-                    this.context.lineWidth = 2
+                    this.renderBlock(this.playfieldX + (x * this.blockWidth), this.playfieldY + (y * this.blockHeight), this.blockWidth, this.blockHeight, View.colors[block])
+                    // this.context.fillStyle = View.colors[block]
+                    // this.context.strokeStyle = "black"
+                    // this.context.lineWidth = 2
 
-                    // Рисуем прямоугольник
-                    this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight)
+                    // // Рисуем прямоугольник
+                    // this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight)
 
-                    // Рисуем обводку
-                    this.context.strokeRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight)
+                    // // Рисуем обводку
+                    // this.context.strokeRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight)
 
                 }
 
             }
 
         }
+
+        this.context.strokeStyle = "white"
+        this.context.lineWidth = this.playfieldBorderWidth
+        this.context.strokeRect(0, 0, this.playfieldWidth, this.playfieldHeight)
     }
+
+    renderBlock(x, y, width, height, color) {
+        this.context.fillStyle = color
+        this.context.strokeStyle = "black"
+        this.context.lineWidth = 2
+
+        // Рисуем прямоугольник - копия
+        this.context.fillRect(x, y, width, height)
+        // Рисуем обводку - копия
+        this.context.strokeRect(x, y, width, height)
+    }
+
+    renderPanel({ level, score, lines, nextPiece }) {
+
+        this.context.fillStyle = "white"
+        this.context.textBaseline = "top"
+        this.context.textAlign = "start"
+        this.context.font = "16px Verdana"
+
+        this.context.fillText(`Score: ${score}`, this.panelX, this.panelY + 0)
+        this.context.fillText(`Lines: ${lines}`, this.panelX, this.panelY + 24)
+        this.context.fillText(`Level: ${level}`, this.panelX, this.panelY + 48)
+        this.context.fillText(`Next:`, this.panelX, this.panelY + 96)
+
+        for (let y = 0; y < nextPiece.blocks.length; y++) {
+            for (let x = 0; x < nextPiece.blocks[y].length; x++) {
+                const block = nextPiece.blocks[y][x]
+
+                if (block) {
+                    this.renderBlock(
+                        this.panelX + (x * this.blockWidth * 0.5),
+                        this.panelY + 100 + (y * this.blockHeight * 0.5),
+                        this.blockWidth * 0.5,
+                        this.blockHeight * 0.5,
+                        View.colors[block]
+                    )
+                }
+            }
+
+        }
+
+    }
+
+    renderStartScreen() {
+        this.context.fillStyle = "white"
+        this.context.font = "18px Verdana"
+        this.context.textAlign = "center"
+        this.context.textBaseline = "middle"
+        this.context.fillText("Press Enter to Start", this.width / 2, this.height / 2)
+
+    }
+
+    renderPauseScreen() {
+        this.context.fillStyle = "rgba(0,0,0,0.75"
+        this.context.fillRect(0, 0, this.width, this.height)
+
+        this.context.fillStyle = "white"
+        this.context.font = "18px Verdana"
+        this.context.textAlign = "center"
+        this.context.textBaseline = "middle"
+        this.context.fillText("Press Enter to Resume", this.width / 2, this.height / 2)
+
+    }
+
+    renderEndScreen({ score }) {
+        this.cleareScrean()
+
+        this.context.fillStyle = "white"
+        this.context.font = "18px Verdana"
+        this.context.textAlign = "center"
+        this.context.textBaseline = "middle"
+
+
+        this.context.fillText("Game Over", this.width / 2, this.height / 2 - 48)
+        this.context.fillText(`Score: ${score}`, this.width / 2, this.height / 2)
+
+    }
+
 }
+
+
+
 
 const game = new Game(); //игровое поле
 const root = document.querySelector("#root");
-const view = new View(root, 320, 640, 20, 10);
+const view = new View(root, 480, 640, 20, 10);
 
 //view.renderPlayfield(game.playfield);
 //console.log(game.playfield);
 
 document.addEventListener("keydown", event => {
     switch (event.which) { // keyCode или which - оба устарели и оба работают - альтернативы незнаю
+        case 13:
+            view.render(game.getState())
+            break
         case 37:
             game.moveIsLeft();
             view.render(game.getState())
@@ -308,10 +484,13 @@ document.addEventListener("keydown", event => {
             view.render(game.getState())
             break
         case 40:
-            game.moveIsDawn();
+            game.moveIsDown();
             view.render(game.getState())
             break
     }
 })
 
-//дз игра змейка по похожему принципц в canvas
+view.render(game.getState())
+// view.renderStartScreen(game.getState())
+// view.renderPauseScreen(game.getState())
+// view.renderEndScreen(game.getState())
